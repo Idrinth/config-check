47e36e60bd8c95978e677568f5caea5601ca788b
Björn Büttner <Idrinth@users.noreply.github.com>
2017-12-14 23:32:33 +0100

    adding messages adding tests for all remaining methods in data

--- test/Data/JsonFileResultTest.php
+++ test/Data/JsonFileResultTest.php
@@ -14,17 +14,17 @@ class JsonFileResultTest extends TestCase
     {
         $instance = new JsonFileResult(__DIR__);
         $title = "\n".__DIR__."\n";
-        $this->assertEquals($title, $instance."");
-        $this->assertEquals($title, $instance->getMessage());
-        $this->assertEquals($title, $instance->getMessage(0));
-        $this->assertEquals($title, $instance->getMessage(1));
-        $this->assertEquals($title, $instance->getMessage(2));
+        $this->assertEquals($title, $instance."", "__toString of empty list failed");
+        $this->assertEquals($title, $instance->getMessage(), "getMessage() of empty list failed");
+        $this->assertEquals($title, $instance->getMessage(0), "getMessage(0) of empty list failed");
+        $this->assertEquals($title, $instance->getMessage(1), "getMessage(1) of empty list failed");
+        $this->assertEquals($title, $instance->getMessage(2), "getMessage(2) of empty list failed");
         $instance->addMessage($this->getMockedMessage(false));
-        $this->assertEquals($title."0", $instance."");
-        $this->assertEquals($title."0", $instance->getMessage());
-        $this->assertEquals($title."0", $instance->getMessage(0));
-        $this->assertEquals($title."1", $instance->getMessage(1));
-        $this->assertEquals($title."2", $instance->getMessage(2));
+        $this->assertEquals($title."0", $instance."", "__toString of filled list failed");
+        $this->assertEquals($title."0", $instance->getMessage(), "getMessage() of filled list failed");
+        $this->assertEquals($title."0", $instance->getMessage(0), "getMessage(0) of filled list failed");
+        $this->assertEquals($title."1", $instance->getMessage(1), "getMessage(1) of filled list failed");
+        $this->assertEquals($title."2", $instance->getMessage(2), "getMessage(2) of filled list failed");
     }
 
     /**
@@ -33,17 +33,21 @@ class JsonFileResultTest extends TestCase
     public function testGetErrorNum()
     {
         $instance = new JsonFileResult(__DIR__);
-        $this->assertEquals(0, $instance->getErrorNum());
-        $this->assertEquals(0, $instance->getErrorNum(true));
+        $this->assertEquals(0, $instance->getErrorNum(), "empty list is not considered empty with default");
+        $this->assertEquals(0, $instance->getErrorNum(false), "empty list is not considered empty with false");
+        $this->assertEquals(0, $instance->getErrorNum(true), "empty list is not considered empty with true");
         $instance->addMessage($this->getMockedMessage(false));
-        $this->assertEquals(0, $instance->getErrorNum());
-        $this->assertEquals(0, $instance->getErrorNum(true));
+        $this->assertEquals(0, $instance->getErrorNum(), "one notice list is not considered empty with default");
+        $this->assertEquals(0, $instance->getErrorNum(false), "one notice list is not considered empty with false");
+        $this->assertEquals(0, $instance->getErrorNum(true), "one notice list is not considered empty with true");
         $instance->addMessage($this->getMockedMessage(true));
-        $this->assertEquals(1, $instance->getErrorNum());
-        $this->assertEquals(1, $instance->getErrorNum(true));
+        $this->assertEquals(1, $instance->getErrorNum(), "notice and error list does not have expected size of 1 with default");
+        $this->assertEquals(1, $instance->getErrorNum(false), "notice and error list does not have expected size of 1 with false");
+        $this->assertEquals(1, $instance->getErrorNum(true), "notice and error list does not have expected size of 1 with true");
         $instance->addMessage($this->getMockedMessage());
-        $this->assertEquals(1, $instance->getErrorNum());
-        $this->assertEquals(2, $instance->getErrorNum(true));
+        $this->assertEquals(1, $instance->getErrorNum(), "notice, warning and error list does not have expected size of 1 with default");
+        $this->assertEquals(1, $instance->getErrorNum(false), "notice, warning and error list does not have expected size of 1 with false");
+        $this->assertEquals(2, $instance->getErrorNum(true), "notice, warning and error list does not have expected size of 2 with true");
     }
 
     /**
--- test/Data/ValidationListTest.php
+++ test/Data/ValidationListTest.php
@@ -1,30 +1,41 @@
 <?php
 
-namespace De\Idrinth\JsonCheck\Data;
+namespace De\Idrinth\JsonCheck\Test\Data;
 
+use De\Idrinth\JsonCheck\Data\ValidationList;
 use PHPUnit\Framework\TestCase;
 
 class ValidationListTest extends TestCase
 {
     /**
-     * @covers De\Idrinth\JsonCheck\Data\ValidationList::addFile
-     * @todo   Implement testAddFile().
+     * @return void
      */
     public function testAddFile()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $instance = new ValidationList();
+        $file = $this->getMock('\De\Idrinth\JsonCheck\Data\JsonFileResult', array(), array(''));
+        $file->expects($this->any())
+            ->method("getErrorNum")
+            ->willReturn(7);
+        $file->expects($this->any())
+            ->method("getMessage")
+            ->willReturn("7 Errors");
+        $instance->addFile($file);
+        $result = $instance->finish();
+        $this->assertCount(2, $result, "output does not provide expected element count");
+        $this->assertEquals(7, $result[0], "a list with 7 errors had none");
+        $this->assertEquals("7 Errors", $result[1], "the list did not provide the predefined text");
     }
 
     /**
-     * @covers De\Idrinth\JsonCheck\Data\ValidationList::finish
-     * @todo   Implement testFinish().
+     * @return void
      */
     public function testFinish()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $instance = new ValidationList();
+        $result = $instance->finish();
+        $this->assertCount(2, $result, "output does not provide expected element count");
+        $this->assertEquals(0, $result[0], "an empty list had errors");
+        $this->assertEquals("", $result[1], "an empty list had text");
     }
 }
