bef87d11c8aeace79fabd0b144ba0869a0122d09
Björn Büttner <Idrinth@users.noreply.github.com>
2017-12-14 22:52:22 +0100

    adding tests for json file result

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/0841f476d70a40e181e7c42685ba979f)](https://www.codacy.com/app/Idrinth/json-check?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Idrinth/json-check&amp;utm_campaign=Badge_Grade)
+[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/0841f476d70a40e181e7c42685ba979f)](https://www.codacy.com/app/Idrinth/json-check?utm_source=github.com&utm_medium=referral&utm_content=Idrinth/json-check&utm_campaign=Badge_Coverage)
+[![Build Status](https://travis-ci.org/Idrinth/json-check.svg?branch=master)](https://travis-ci.org/Idrinth/json-check)
+
+This project is checking any project's json files for correctness, including schema validation once implemented.
--- test/Data/JsonFileResultTest.php
+++ test/Data/JsonFileResultTest.php
@@ -1,42 +1,72 @@
 <?php
 
-namespace De\Idrinth\JsonCheck\Data;
+namespace De\Idrinth\JsonCheck\TestData;
 
+use De\Idrinth\JsonCheck\Data\JsonFileResult;
 use PHPUnit\Framework\TestCase;
 
 class JsonFileResultTest extends TestCase
 {
     /**
-     * @covers De\Idrinth\JsonCheck\Data\JsonFileResult::addMessage
-     * @todo   Implement testAddMessage().
+     * @return void
      */
-    public function testAddMessage()
+    public function testAddGetMessage()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $instance = new JsonFileResult(__DIR__);
+        $title = "\n".__DIR__."\n";
+        $this->assertEquals($title, $instance."");
+        $this->assertEquals($title, $instance->getMessage());
+        $this->assertEquals($title, $instance->getMessage(0));
+        $this->assertEquals($title, $instance->getMessage(1));
+        $this->assertEquals($title, $instance->getMessage(2));
+        $instance->addMessage($this->getMockedMessage(false));
+        $this->assertEquals($title."0", $instance."");
+        $this->assertEquals($title."0", $instance->getMessage());
+        $this->assertEquals($title."0", $instance->getMessage(0));
+        $this->assertEquals($title."1", $instance->getMessage(1));
+        $this->assertEquals($title."2", $instance->getMessage(2));
     }
 
     /**
-     * @covers De\Idrinth\JsonCheck\Data\JsonFileResult::getMessage
-     * @covers De\Idrinth\JsonCheck\Data\JsonFileResult::__toString
-     * @todo   Implement testGetMessage().
-     */
-    public function testGetMessage()
-    {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers De\Idrinth\JsonCheck\Data\JsonFileResult::getErrorNum
-     * @todo   Implement testGetErrorNum().
+     * @creturn null
      */
     public function testGetErrorNum()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $instance = new JsonFileResult(__DIR__);
+        $this->assertEquals(0, $instance->getErrorNum());
+        $this->assertEquals(0, $instance->getErrorNum(true));
+        $instance->addMessage($this->getMockedMessage(false));
+        $this->assertEquals(0, $instance->getErrorNum());
+        $this->assertEquals(0, $instance->getErrorNum(true));
+        $instance->addMessage($this->getMockedMessage(true));
+        $this->assertEquals(1, $instance->getErrorNum());
+        $this->assertEquals(1, $instance->getErrorNum(true));
+        $instance->addMessage($this->getMockedMessage());
+        $this->assertEquals(1, $instance->getErrorNum());
+        $this->assertEquals(2, $instance->getErrorNum(true));
+    }
+
+    /**
+     * @param null|boolean $return
+     * @return Message (mocked)
+     */
+    private function getMockedMessage($return = null) {
+        $message = $this->getMock('\De\Idrinth\JsonCheck\Message');
+        if($return === null) {
+            $message->expects($this->any())
+                ->method("isFailure")
+                ->willReturnArgument(0);
+        } else {
+            $message->expects($this->any())
+                ->method("isFailure")
+                ->willReturn($return);
+        }
+        $message->expects($this->any())
+            ->method("__toString")
+            ->willReturn("_");
+        $message->expects($this->any())
+            ->method("toString")
+            ->willReturnArgument(0);
+        return $message;
     }
 }
