1a10d9dc29de8f0ab407e98de3b7d23e85d48122
Björn Büttner <idrinth@users.noreply.github.com>
2017-12-15 17:12:17 +0100

    adding some tests for file validation

--- src/Service/FileValidator.php
+++ src/Service/FileValidator.php
@@ -7,6 +7,7 @@ use De\Idrinth\JsonCheck\Message\WarningMessage;
 use Jsv4;
 use SchemaStore;
 use SplFileInfo;
+use stdClass;
 
 class FileValidator
 {
@@ -30,29 +31,22 @@ class FileValidator
     public function check(SplFileInfo $file)
     {
         $results = array();
-        if(!$file->isFile() || $file->getSize() === 0) {
-            $results[] = new WarningMessage("File is empty");
+        if(!$this->isFileUseable($results, $file)) {
             return $results;
         }
-        if(!$file->isReadable()) {
-            $results[] = new ErrorMessage("File is not readable");
+        $json = json_decode($file->openFile()->fread($file->getSize()));
+        if(!$this->isContentSchemaJson($results, $json)) {
             return $results;
         }
-        $json = json_decode(file_get_contents($file->getPathname()));
-        if($json === null) {
-            $results[] = new ErrorMessage("File is not parseable: ".json_last_error_msg());
-            return $results;
-        } else {
-            $results[] = new NoticeMessage("File is parseable");
-        }
-        if(!is_object($json) || !property_exists($json, '$schema')) {
-            $results[] = new NoticeMessage("No schema provided");
-            return $results;
-        }
-        if(!$this->schemas->get($json->{'$schema'})) {
-            $results[] = new WarningMessage("Schema not avaible");
-            return $results;
-        }
+        return $this->validateJson($results, $json);
+    }
+
+    /**
+     * @param Message[] $results
+     * @param stdClass $json
+     * @return Message[]
+     */
+    private function validateJson(array &$results, stdClass $json) {
         $validator = new Jsv4($json, $this->schemas->get($json->{'$schema'}));
         if(!count($validator->errors)) {
             $results[] = new NoticeMessage("Content matched schema");
@@ -62,5 +56,44 @@ class FileValidator
             $results[] = new ErrorMessage($error->message."($error->code $error->schemaPath cmp. $error->dataPath)");
         }
         return $results;
+    }
+
+    /**
+     * @param Message[] $results Reference!
+     * @param SplFileInfo $file
+     * @return boolean
+     */
+    private function isFileUseable(array &$results, SplFileInfo $file) {
+        if(!$file->isFile() || $file->getSize() === 0) {
+            $results[] = new WarningMessage("File is empty");
+            return false;
+        }
+        if(!$file->isReadable()) {
+            $results[] = new ErrorMessage("File is not readable");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param Message[] $results Reference!
+     * @param stdClass $json
+     * @return boolean
+     */
+    private function isContentSchemaJson(array &$results, $json = null) {
+        if($json === null) {
+            $results[] = new ErrorMessage("File is not parseable: ".json_last_error_msg());
+            return false;
+        }
+        $results[] = new NoticeMessage("File is parseable");
+        if(!is_object($json) || !property_exists($json, '$schema')) {
+            $results[] = new NoticeMessage("No schema provided");
+            return false;
+        }
+        if(!$this->schemas->get($json->{'$schema'})) {
+            $results[] = new WarningMessage("Schema not avaible");
+            return false;
+        }
+        return true;
     }
 }
--- test/Service/FileValidatorTest.php
+++ test/Service/FileValidatorTest.php
@@ -7,13 +7,153 @@ use PHPUnit\Framework\TestCase;
 class FileValidatorTest extends TestCase
 {
     /**
-     * @covers De\Idrinth\JsonCheck\Service\FileValidator::check
-     * @todo   Implement testCheck().
+     * @return void
      */
-    public function testCheck()
+    public function testCheckNotFile()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $file = $this->getMockBuilder('SplFileObject')
+            ->setConstructorArgs(array(__FILE__))
+            ->getMock();
+        $file->expects($this->any())
+             ->method('isFile')
+            ->willReturn(false);
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(1, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\WarningMessage', $return[0], "empty files are not warnings");
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckEmptyFile()
+    {
+        $file = $this->getMockBuilder('SplFileObject')
+            ->setConstructorArgs(array(__FILE__))
+            ->getMock();
+        $file->expects($this->any())
+             ->method('isFile')
+            ->willReturn(true);
+        $file->expects($this->any())
+             ->method('getSize')
+            ->willReturn(0);
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(1, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\WarningMessage', $return[0], "empty files are not warnings");
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckUnreadableFile()
+    {
+        $file = $this->getMockBuilder('SplFileObject')
+            ->setConstructorArgs(array(__FILE__))
+            ->getMock();
+        $file->expects($this->any())
+             ->method('isFile')
+            ->willReturn(true);
+        $file->expects($this->any())
+             ->method('getSize')
+            ->willReturn(1);
+        $file->expects($this->any())
+             ->method('isReadable')
+            ->willReturn(false);
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(1, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\ErrorMessage', $return[0], "unreadable files are not errors");
+    }
+
+    /**
+     * @param string $content
+     * @return SplFileObject
+     */
+    private function getValidFileMock($content) {
+        $file = $this->getMockBuilder('SplFileObject')
+            ->setConstructorArgs(array(__FILE__))
+            ->getMock();
+        $file->expects($this->any())
+             ->method('isFile')
+            ->willReturn(true);
+        $file->expects($this->any())
+             ->method('getSize')
+            ->willReturn(1);
+        $file->expects($this->any())
+             ->method('isReadable')
+            ->willReturn(true);
+        $file->expects($this->any())
+             ->method('openFile')
+            ->willReturnSelf();
+        $file->expects($this->any())
+             ->method('fread')
+            ->willReturn($content);
+        return $file;
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckUnparsableJson()
+    {
+        $file = $this->getValidFileMock("broken");
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(1, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\ErrorMessage', $return[0], "broken files are not considered errors");
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckNoObjectJson()
+    {
+        $file = $this->getValidFileMock("[\"\"]");
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(2, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\NoticeMessage', $return[0], "Having a parseable file is not a notice");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\NoticeMessage', $return[1], "Lack of schema is not a notice");
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckNoSchemaJson()
+    {
+        $file = $this->getValidFileMock("{\"\":\"\"}");
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(2, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\NoticeMessage', $return[0], "Having a parseable file is not a notice");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\NoticeMessage', $return[1], "Lack of schema is not a notice");
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckMissingSchemaJson()
+    {
+        $file = $this->getValidFileMock("{\"\$schema\":\"http://example.schema\"}");
+        $store = $this->getMockBuilder('SchemaStore')
+            ->getMock();
+        $instance = new \De\Idrinth\JsonCheck\Service\FileValidator($store);
+        $return = $instance->check($file);
+        $this->assertCount(2, $return, "there were less messages returned than expected");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\NoticeMessage', $return[0], "Having a parseable file is not a notice");
+        $this->assertInstanceOf('De\Idrinth\JsonCheck\Message\WarningMessage', $return[1], "Unknown schema is not a warning");
     }
 }
