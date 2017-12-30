2d91992063c36b56c4283faf4d11d9d99b82e418
Björn Büttner <Idrinth@users.noreply.github.com>
2017-12-30 13:14:25 +0100

    Update check-config.php

--- check-config.php
+++ check-config.php
@@ -1,9 +1,10 @@
+#!/usr/bin/php
 <?php
 
 use De\Idrinth\ConfigCheck\Controller;
 use De\Idrinth\ConfigCheck\Data\Config;
 
-require_once 'vendor/autoload.php';
+require_once is_file('vendor/autoload.php') ? 'vendor/autoload.php' : dirname(dirname(__DIR__)).'autoload.php';
 
 $controller = new Controller(new Config(getcwd(), getopt('vw')));
 echo $controller->getText();
